1b457dba9bb2e35dec628365f6f8055bc0e182ed
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2025-01-03 23:29:32 -0300

    Adiciona remove da section secaoDados se ela já existir e troca gtElement por querySelector e .onclick por eventListener.

--- front_end/script.js
+++ front_end/script.js
@@ -1,8 +1,8 @@
 function pegarValorInput() {
     // Obtém o valor digitado no campo de input
-    const dataInicio = document.getElementById("dataInicio").value;
-    const dataFim = document.getElementById("dataFim").value;
-    const radios = document.getElementsByName("opcao");
+    const dataInicio = document.querySelector("#dataInicio").value;
+    const dataFim = document.querySelector("#dataFim").value;
+    const radios = document.querySelectorAll('[name="opcao"]');
     var mesTempMed = null;
 
     for(let opcao of radios){
@@ -10,35 +10,41 @@ function pegarValorInput() {
             mesTempMed = opcao.value;
         };
     };
-    
-    let dados = [1,2,3,4,5,6,7]
 
-    document.getElementById('divDados').innerHTML = '<section id="sectionDados"></section>';
+//______________ Cria nova Section" _______________    
+
+    //Apaga a section se ela já existir
+    var issecaoDados = document.querySelector('#secaoDados');
+    if(issecaoDados) {
+        console.log(issecaoDados);
+        document.querySelector('#secaoDados').remove();
+        console.log(`Apagado: ${document.querySelector('#secaoDados')}`);
+    };
+    
+
+    var main = document.querySelector("main");
+    var secaoDados = document.createElement('section');
+    secaoDados.id = "secaoDados";    
+    main.appendChild(secaoDados);
+
+//______________________________
+
+    // Testa criação de parágrafos
+    let dados = [1,2,3,4,5,6,7];
 
     for(let dado of dados){
-        let valorAtual = document.getElementById('sectionDados').innerHTML
-        document.getElementById('sectionDados').innerHTML = valorAtual + `<p id="dados" >Novo conteúdo: ${dado}</p>`;
+        var paragrafo = document.createElement('p');
+        paragrafo.classList.add("dados"); 
+        paragrafo.textContent = `Novo conteúdo: ${dado}`
+        secaoDados.appendChild(paragrafo);
     }
 
-    //document.getElementById('divDados').innerHTML = '<section id="sectionDados"><p id="dados" >Novo conteúdo</p></section>';
-
-
-    // Exibe o valor no console ou faz algo com ele
     console.log(`Data Inicio: ${dataInicio}; Data Fim: ${dataFim}`);
-
-    // Você pode também exibir o valor em um alerta ou em outro lugar na página
     alert(`Data Inicio: ${dataInicio}; Data Fim: ${dataFim}, Mes Temp Média: ${mesTempMed}`);
 }
 
-function printar(){
-    console.log("Funcionou.");
-};
-
-function ambasFuncoes(){
-    pegarValorInput();
-    printar();
-};
 
 // Adiciona um ouvinte de evento para o clique do botão
-document.getElementById("botaoSubmit").onclick = ambasFuncoes;
+let submit = document.querySelector("#botaoSubmit");
 
+submit.addEventListener("click", pegarValorInput);
